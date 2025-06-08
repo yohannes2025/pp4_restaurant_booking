d841fb7b1423783aef0f2f6cf50f4c371642ebdc
yohannes2025 <johannes.m.tekle@gmail.com>
2025-06-08 22:29:36 +0200

    102. Configure PostgreSQL for Heroku deployment

--- restaurant_booking_project/settings.py
+++ restaurant_booking_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
 from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
