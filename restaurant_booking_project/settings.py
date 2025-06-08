dd7986b5d4b8e3103b75bc27eae1028281c94cc6
yohannes2025 <johannes.m.tekle@gmail.com>
2025-06-08 22:38:07 +0200

    103. Switch to PostgreSQL on Heroku using dj_database_url

--- restaurant_booking_project/settings.py
+++ restaurant_booking_project/settings.py
@@ -105,12 +105,12 @@ WSGI_APPLICATION = 'restaurant_booking_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local dev
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
